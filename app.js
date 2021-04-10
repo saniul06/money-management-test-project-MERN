ce52a47a070d59af27b10a0904008881e7794478
rumel <saniul428@gmail.com>
2021-04-10 13:20:54 +0600

    asfd

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 let a = 'i am in master';
 
-function first (){
-    console.log('first')
-let a = 'i am in a';
 
-function first (){
-    console.log('first')
-    console.log('i am in a')
-}
+
 
 function second(){
     console.log('second')
